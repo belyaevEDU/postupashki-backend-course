6b375f4300bf21518ca1e3c901b094877cd00b63
belyaevEDU <veniamin@belyaev.work>
2026-07-19 14:29:07 +0300

    hw0 (#1)
    * chore: added an environment-specific gitignore
    
    * feat: implemented the server
    
    * feat: implemented the client
    
    * fix(server): removed the spaces at the end of all hardcoded error messages
    
    since the separator is already a space by default
    
    * fix(client): replaced net.Conn.Read with io.ReadAll, now accounting for the error
    
    * fix(client): now checking the error raised by net.Conn.Close
    
    * fix(client): added trimright to the response output
    
    * feat(server): added graceful shutdown
    
    * fix(client): added missed error output & exit code 1 when raising an error when closing the connection
    
    * fix(server): added continue to case when an error raised when accepting a connection and the context isnt done

---
+++ start/hw0/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+)
+
+const (
+	address    = "localhost:8080"
+	response   = "OK\n"
+	trimCutset = "\n "
+)
+
+func main() {
+	connection, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println("error raised when dialing the address:", err)
+		return
+	}
+
+	defer func() {
+		err := connection.Close()
+		if err != nil {
+			fmt.Println("error raised when closing the connection:", err)
+			os.Exit(1)
+		}
+	}()
+
+	reader := bufio.NewReader(connection)
+	buffer, err := io.ReadAll(reader)
+
+	if err != nil {
+		fmt.Println("error raised when reading:", err)
+	}
+
+	if string(buffer) == response {
+		fmt.Println("The server returned the expected response:", strings.TrimRight(response, trimCutset))
+	} else {
+		fmt.Println("The server returned an unexpected response:", strings.TrimRight(string(buffer), trimCutset))
+	}
+}
---
+++ start/hw0/server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+const (
+	address  = ":8080"
+	response = "OK\n"
+)
+
+func main() {
+	ctx, stopFunc := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stopFunc()
+
+	var workGroup sync.WaitGroup
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("error raised creating a listener:", err)
+		return
+	}
+
+	go func() {
+		<-ctx.Done()
+		err := listener.Close()
+		if err != nil {
+			fmt.Println("error closing listener:", err)
+		}
+	}()
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			select {
+			case <-ctx.Done():
+
+			default:
+				fmt.Println("error raised accepting a connection:", err)
+				continue
+			}
+			break
+		}
+
+		workGroup.Go(func() {
+			handleConnection(connection)
+		})
+	}
+
+	fmt.Println("Waiting..")
+	workGroup.Wait()
+	fmt.Println("Closed")
+}
+
+func handleConnection(connection net.Conn) {
+	defer func() {
+		err := connection.Close()
+		if err != nil {
+			fmt.Println("error closing a connection:", err)
+		}
+	}()
+
+	_, err := connection.Write([]byte(response))
+	if err != nil {
+		fmt.Println("error raised when writing a response:", err)
+	}
+}
