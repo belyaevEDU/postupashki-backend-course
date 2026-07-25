b03d30067fb305bc88766ac1ff592d413f9091d1
belyaevEDU <veniamin@belyaev.work>
2026-07-25 21:52:36 +0300

    func(helpers): added function for url validation

--- start/hw1/helpers.go
+++ start/hw1/helpers.go
@@ -1,6 +1,12 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"regexp"
+)
+
+const urlRegex string = `/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/`
 
 func errorIsTimeout(err error) bool {
 	if err == nil {
@@ -9,4 +15,13 @@ func errorIsTimeout(err error) bool {
 
 	errNetError, ok := err.(net.Error)
 	return ok && errNetError.Timeout()
+}
+
+func validateUrl(url string) (bool, error) {
+	regex, err := regexp.Compile(urlRegex)
+	if err != nil {
+		return false, fmt.Errorf("error raised when compiling regex: %w", err)
+	}
+
+	return regex.MatchString(url), nil
 }
