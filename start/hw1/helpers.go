66163d0046b03ab2aaa6f1deecf1cf814f3bbd06
belyaevEDU <veniamin@belyaev.work>
2026-07-26 04:28:09 +0300

    fix(helpers): replaced bad url regex

--- start/hw1/helpers.go
+++ start/hw1/helpers.go
@@ -6,7 +6,7 @@ import (
 	"regexp"
 )
 
-const urlRegex string = `/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/`
+const urlRegex string = `^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|\/|\/\/)?[A-z0-9_-]*?[:]?[A-z0-9_-]*?[@]?[A-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$`
 
 func errorIsTimeout(err error) bool {
 	if err == nil {
